a1d20dd55238ce045b75414692ff84ed33c8d111
Senhua <senhua.liu@efrei.net>
2023-11-22 11:31:23 +0100

    name

--- python/tests/test_safe_calculator_with_mock.py
+++ python/tests/test_safe_calculator_with_mock.py
@@ -5,7 +5,7 @@ from python.safe_calculator import SafeCalculator
 
 class TestSafeCalculator(unittest.TestCase):
 
-    def test_add_should_raise_exception_when_authorized(self):
+    def test_add_should_raise_exception_when_authorized_with_mock(self):
         # We mock the authorizer to always return True
         mock_authorizer = Mock()
         mock_authorizer.authorize.return_value = True

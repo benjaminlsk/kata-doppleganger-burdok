fbae6d51822166138ff1cddb4c6ce1e3d01a54ae
Senhua <senhua.liu@efrei.net>
2023-11-22 10:21:53 +0100

    test for safe calculator

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,7 +1,14 @@
-import pytest
+from safe_calculator import SafeCalculator
+
+
+class SimpleAuthorizer:
+    def authorize(self):
+        return True
 
 
 def test_divide_should_not_raise_any_error_when_authorized():
     # TODO: write a test that fails due to the bug in
     # SafeCalculator.add
-    pass
+    calculator = SafeCalculator(authorizer=SimpleAuthorizer())
+    result = calculator.add(1, 2)
+    assert result == 3
